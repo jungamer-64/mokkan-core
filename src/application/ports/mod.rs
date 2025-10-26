b9ede2a4015daaa3b4030c65796cee2a4e0b5bd4
jungamer-64 <79193245+jungamer-64@users.noreply.github.com>
2025-10-27 04:35:52 +0900

    application: add AuthorizationCode port and store trait

--- src/application/ports/mod.rs
+++ src/application/ports/mod.rs
@@ -3,9 +3,11 @@ pub mod security;
 pub mod time;
 pub mod util;
 pub mod session_revocation;
+pub mod authorization_code;
 
 // Type aliases to make port injection sites more descriptive and reduce `dyn` noise
 pub type PasswordHasherPort = dyn security::PasswordHasher;
 pub type TokenManagerPort = dyn security::TokenManager;
 pub type ClockPort = dyn time::Clock;
 pub type SlugGeneratorPort = dyn util::SlugGenerator;
+pub type AuthorizationCodeStorePort = dyn authorization_code::AuthorizationCodeStore;
